9761dd4c253a6f2cdff69630d7753f1b85e89bcf
ItzAlex <56042532+ItzAlex1@users.noreply.github.com>
2020-03-13 09:41:23 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let Luxcord = require("luxcord")
+
+.init({
+  // Luxcord initialization options
+  token: "Njc5NjcwMjI0MjczODAxMjE3.XmQWJw.QOcQsOsKUmuM-Hb8hcZDf_7BJIw",
+  prefix: "!",
+  ownerID: "656982678301573159",
+  color: 0x4488ff,
+  verbose: true,
+  addons: ["eval", "help", "customMessages"],
+
+  // custom variables
+  muteRoleID: "585412190295687184",
+  countChannelID: "680097076230881280",
+  verifyGuildID: "566321685385445406",
+  verifyRoleID: "585412190295687184",
+  statusInterval: 1000*60*5
+})
